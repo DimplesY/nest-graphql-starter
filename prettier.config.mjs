b3fa8894952bc4c11312d3b910bbb073f12f4bb3
Dimples_YJ <2890841438@qq.com>
2024-12-21 23:50:28 +0800

    feat: add prettier-plugin-prisma

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: false,
+  plugins: ['prettier-plugin-prisma'],
+}
+
+export default config
